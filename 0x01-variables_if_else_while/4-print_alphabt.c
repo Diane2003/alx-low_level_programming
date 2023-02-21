f157d831cd102e97543021e1c2e478e091776c36
root <udiane2003@gmail.com>
2023-02-21 07:24:19 -0800

    4print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,23 +2,18 @@
 
 /**
  * main - entry point
- * Return: always 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	char lcase = 'a';
-	char ucase = 'A';
 
-	while (lcase <= 'z') /* runs each lowercase letter a, b, c, ...z*/
+	while (lcase <= 'z')
 	{
-		putchar(lcase); /*output a char to stdout */
-		lcase++;  /*keeps incrementing by one char until it reaches 'Z'*/
+		if (lcase != 'e' && lcase != 'q')
+			putchar (lcase);
+		lcase++;
 	}
-	while (ucase <= 'Z') /* runs each lowercase letter a, b, c, ...z*/
-	{
-		putchar(ucase); /*prints a char to stdout */
-		ucase++; /*keeps incrementing by one char until it reaches 'Z'*/
-	}
-	putchar('\n'); /*add newline at end*/
+	putchar ('\n');
 	return (0);
 }
