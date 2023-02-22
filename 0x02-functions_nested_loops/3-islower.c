c0011d48f25d4e28298c3919f94bcbb02507427c
root <udiane2003@gmail.com>
2023-02-22 10:35:54 -0800

    print low case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
- * main - check the code.
+ * _islower - check if the letter is in lower case.
+ * @c: The letter to be checked
  *
- * Return: Always 0.
+ * Return: 1 (True) else 0 (False)
  */
-int main(void)
+int _islower(int c)
 {
-    int r;
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	return (0);
 }
