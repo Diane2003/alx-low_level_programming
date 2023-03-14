60671a8471dc15e4cd6e9073d26691537f501068
root <udiane2003@gmail.com>
2023-03-14 10:56:10 -0700

    fixing error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -42,4 +42,3 @@ int **alloc_grid(int width, int height)
 			grid[i][j] = 0;
 
 	return (grid);
-
--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -3,48 +3,6 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s;
-
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
-julien@ubuntu:~/0x0a. malloc, free$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-str_concat.c -o c
-julien@ubuntu:~/c/curriculum_by_julien/holbertonschool-low_level_programming/0x0a. malloc, free$ ./c | cat -e
-Betty Holberton$
-julien@ubuntu:~/c/curriculum_by_julien/holbertonschool-low_level_programming/0x0a. malloc, free$ 
-Repo:
-
-GitHub repository: alx-low_level_programming
-Directory: 0x0B-malloc_free
-File: 2-str_concat.c
-   
-3. If you even dream of beating me you'd better wake up and apologize
-mandatory
-Write a function that returns a pointer to a 2 dimensional array of integers.
-
-Prototype: int **alloc_grid(int width, int height);
-Each element of the grid should be initialized to 0
-The function should return NULL on failure
-If width or height is 0 or negative, return NULL
-julien@ubuntu:~/0x0a. malloc, free$ cat 3-main.c
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
  * print_grid - prints a grid of integers
  * @grid: the address of the two dimensional grid
  * @width: width of the grid
