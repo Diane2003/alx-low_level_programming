0a974981fbd419608a278668332f234078b45814
root <udiane2003@gmail.com>
2023-03-20 10:21:26 -0700

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/**
-* main - prints the name of the file
-*
-* Return: Always 0 (Success)
-*/
-int ain(void)
-{
-       printf("%s\n", __FILE__);
-       return (0);
-}
 
+/**
+  * main - prints the name of the file it was compiled from.
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
